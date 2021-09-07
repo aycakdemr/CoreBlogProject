a8207fa07fec71bf5b95dc255341302e809e6b57
Ayça Akdemir <mabeaaa@gmail.com>
2021-09-07 14:24:23 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Comment
+    {
+        public int CommentId { get; set; }
+        public string CommentUserName { get; set; }
+        public string CommentContent { get; set; }
+        public string CommentTitle { get; set; }
+        public DateTime CommentDate { get; set; }
+        public bool CommentStatus { get; set; }
+    }
+}
