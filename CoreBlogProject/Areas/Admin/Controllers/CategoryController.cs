11eed0221e589e0db9cd15834baab70e41361ea9
Ayça Akdemir <mabeaaa@gmail.com>
2021-11-29 19:24:21 +0300

    29.11

--- CoreBlogProject/Areas/Admin/Controllers/CategoryController.cs
+++ CoreBlogProject/Areas/Admin/Controllers/CategoryController.cs
@@ -1,17 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using X.PagedList;
 
 namespace CoreBlogProject.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [AllowAnonymous]
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        CategoryManager cm = new CategoryManager(new EfCategoryRepository());
+        public IActionResult Index(int page=1)
+        {
+            var v = cm.GetAll().ToPagedList(page,3);
+
+            return View(v);
+        }
+        [HttpGet]
+        public IActionResult CategoryAdd()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult CategoryAdd(Category c)
+        {
+            c.CategoryStatus = true;
+            cm.Add(c);
+            return RedirectToAction("Index", "Category");
+        }
+        public IActionResult CategoryDelete(int id)
+        {
+            var value = cm.GetById(id);
+            cm.Delete(value);
+            return RedirectToAction("Index");
         }
     }
 }
