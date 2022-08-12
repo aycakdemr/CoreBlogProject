3b4ba209722aff7b225a00d184b950042203a47a
Ayça Akdemir <mabeaaa@gmail.com>
2022-08-12 16:33:46 +0300

    register sayfası oluşturuldu

---
+++ CoreBlogProject/Models/UserSignUpViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreBlogProject.Models
+{
+    public class UserSignUpViewModel
+    {
+        [Display(Name ="Ad Soyad")]
+        [Required(ErrorMessage ="ad soyad giriniz")]
+        public string NameSurname { get; set; }
+
+        [Display(Name = "Şifre ")]
+        [Required(ErrorMessage = "şifre giriniz")]
+        public string Password { get; set; }
+
+        [Display(Name = "Şifre Tekrar ")]
+        [Compare("Password" , ErrorMessage ="Şifreler uyuşmuyor")]
+        public string ConfirmPassword { get; set; }
+
+        [Display(Name = "Mail Adresi ")]
+        [Required(ErrorMessage = "mail giriniz")]
+        public string Mail { get; set; }
+        [Display(Name = "Kullanıcı Adı ")]
+        [Required(ErrorMessage = "kullanıcı adı giriniz")]
+        public string UserName { get; set; }
+
+    }
+}
--- CoreBlogProject/Startup.cs
+++ CoreBlogProject/Startup.cs
@@ -1,3 +1,5 @@
+using BlogApiDemo.DataAccessLayer;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
@@ -26,6 +28,16 @@ namespace CoreBlogProject
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<Context>();
+            services.AddIdentity<AppUser, AppRole>(x =>
+            {
+                x.Password.RequireUppercase = false;
+                x.Password.RequireNonAlphanumeric = false;
+            }
+            )
+
+
+                .AddEntityFrameworkStores<Context>();
             services.AddControllersWithViews();
             services.AddSession();
             services.AddMvc(config =>
